30f968f481e7c170984e3aa9f4d79f17e3f77e56
Syed ALi Zain <syedalizain70@gmail.com>
2022-01-08 13:55:58 +0500

    Comment added in local MainActivity at last

--- app/src/main/java/com/multi/zscientifccalculator/MainActivity.java
+++ app/src/main/java/com/multi/zscientifccalculator/MainActivity.java
@@ -166,5 +166,6 @@ public class MainActivity extends AppCompatActivity {
     }
     public void xSquaredBTNPush(View view){
         updateText("^(2)");
+	//Sample checking local for github
     }
 }
